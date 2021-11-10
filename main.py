402fff4a2c35ee5bb9618e07a09117606481809a
tyler <tyler@cruftbusters.com>
2021-11-09 21:44:53 -0600

    every tick attaches dummy image to new heightmaps

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('hello world')
+import json, requests, time
+
+
+def main():
+    while True:
+        tick()
+        time.sleep(2.5)
+
+
+def tick():
+    response = requests.get("https://gallery.painkillergis.com/v1/heightmaps")
+    heightmaps = response.json()
+
+    for heightmap in heightmaps:
+        if heightmap["imageURL"] == "":
+            process(heightmap)
+
+
+def process(heightmap):
+    requests.patch(
+        f"https://gallery.painkillergis.com/v1/heightmaps/{heightmap['id']}",
+        json.dumps({"imageURL": "https://painkillergis.com/1352x955.jpg"}),
+    )
+
+
+main()
