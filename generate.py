3a582f78b6bc468377c47e09d0faee63d61ca58c
tyler <tyler@cruftbusters.com>
2021-11-11 16:40:33 -0600

    wrap generate in CLI

--- generate.py
+++ generate.py
@@ -1,7 +1,14 @@
+import json
+import sys
+
 import requests
 
 dummyPayload = requests.get("https://painkillergis.com/1352x955.jpg").content
 
 
-def generate(heightmap):
+def generate(metadata):
     return dummyPayload
+
+
+if __name__ == "__main__":
+    sys.stdout.buffer.write(generate(json.load(sys.stdin)))
