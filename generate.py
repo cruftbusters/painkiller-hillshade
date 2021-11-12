627328c593619043895bea1e9663f7fa8f30fac6
tyler <tyler@cruftbusters.com>
2021-11-11 19:32:07 -0600

    generate heightmap respecting either position or bounds

--- generate.py
+++ generate.py
@@ -6,25 +6,46 @@ from osgeo import gdal, gdalconst
 
 
 def generate(sourcePath, metadata):
-    output = tempfile.mktemp()
+    left = metadata['position']['left']
+    top = metadata['position']['top']
+    right = left + 1
+    bottom = top - 1
+    if left == 0 and top == 0:
+        left = metadata['bounds']['left']
+        top = metadata['bounds']['top']
+        right = metadata['bounds']['right']
+        bottom = metadata['bounds']['bottom']
+
     source = gdal.Open(sourcePath)
     band = source.GetRasterBand(1)
     stats = band.GetStatistics(True, True)
     minimum = stats[0]
     maximum = stats[1]
-    gdal.Translate(
-        output,
-        source,
-        options=gdal.TranslateOptions(
-            format='JPEG',
-            outputType=gdalconst.GDT_Byte,
-            scaleParams=[[minimum, maximum, 0, 255]],
 
+    warp = tempfile.mktemp()
+    translate = tempfile.mktemp()
+
+    gdal.Warp(
+        warp,
+        source,
+        options=gdal.WarpOptions(
+            outputBounds=[left, top, right, bottom],
+            outputBoundsSRS="EPSG:4326",
             width=metadata['size']['width'],
             height=metadata['size']['height'],
         )
     )
-    return output
+
+    gdal.Translate(
+        translate,
+        warp,
+        options=gdal.TranslateOptions(
+            format='JPEG',
+            outputType=gdalconst.GDT_Byte,
+            scaleParams=[[minimum, maximum, 0, 255]],
+        )
+    )
+    return translate
 
 
 if __name__ == "__main__":
