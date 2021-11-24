f908b247c7ce49ed1c267444127a4548e108f309
tyler <tyler@cruftbusters.com>
2021-11-24 14:22:47 -0600

    pull hillshade work via websocket

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
+import json
 import os
-import sys
 
+import asyncio
 import requests
-import time
+
+import websockets
 
 from generate import generate
 
 
-def main():
-    while True:
-        tick("https://layouts.painkillergis.com")
-        time.sleep(2.5)
+async def main():
+    async with websockets.connect("wss://layouts.painkillergis.com/v1/awaiting_hillshade") as ws:
+        while True:
+            await ws.send("")
+            layout = json.loads(await ws.recv())
+            hillshade = generate(layout)
+            with open(hillshade, 'rb') as f:
+                requests.put(f"https://layouts.painkillergis.com/v1/layouts/{layout['id']}/hillshade.jpg", f.read())
+            os.remove(hillshade)
+            await ws.send("")
 
 
-def tick(baseURL):
-    try:
-        response = requests.get(f"{baseURL}/v1/layouts?withHeightmapWithoutHillshade=true")
-        if response.status_code == 200:
-            for layout in response.json():
-                hillshade = generate(layout)
-                with open(hillshade, 'rb') as f:
-                    requests.put(f"{baseURL}/v1/layouts/{layout['id']}/hillshade.jpg", f.read())
-                os.remove(hillshade)
-        else:
-            print(
-                "Failed to communicate with layout service",
-                f"got status code: {response.status_code}",
-                file=sys.stderr,
-            )
-    except requests.exceptions.RequestException as e:
-        print("Failed to communicate with layout service", e, file=sys.stderr)
-
-
-main()
+asyncio.run(main())
